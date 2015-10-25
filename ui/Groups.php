5c6f20174e23b19a9312d606793c6699d2968f2d
YeongChai-07 <butchercai@gmail.com>
2015-10-25 21:38:45 +0800

    Created a PHP file for Groups Main Page with the partial page layout (HTML).

---
+++ ui/Groups.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <title>Groups</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	</head>
+	<body class="container-fluid">
+		<ul class="nav nav-tabs">
+		    <li><a href="./ItemList.php">Item</a></li>
+			<li><a href="./ShoppingList.html">Shopping</a></li>
+			<li class="active"><a href="#">Groups</a></li>
+			<li><a href="./Settings.html">Settings</a></li>
+		</ul>
+		<div class="container">
+		    <br/><br/><br/><br/>
+		    ====== Content Here ========
+		    Current Groups
+		    <table border = "1">
+		      <tr>
+		        <th>BANG BANG BANG</th>
+		        <th>&nbsp;</th>
+		      </tr>
+		      <tr>
+		        <td>Family</td>
+		        <td class ="glyphicon glyphicon-remove">
+		          <!-- Change X to icon -->
+		        </td>
+		      </tr>
+		      <tr>
+		        <td>Friends</td>
+		        <td class="glyphicon glyphicon-remove">
+		          <!-- Change X to icon -->
+		        </td>
+		      </tr>
+		      <tr>
+		        <td>&nbsp;</td>
+		        <td>&nbsp;</td>
+		      </tr>
+		    </table>
+		    <p>
+		      Received Invitations
+		    <table border = "1">
+		      <tr>
+		        <th>Group Name</th>
+		        <th>Email Address</th>
+		        <th>Accept Invitation</th>
+		        <th>Decline Invitation</th>
+		      </tr>
+		      <tr>
+		        <td>Family</td>
+		        <td>
+		          abc@email.com
+		        </td>
+		        <td><span class="glyphicon glyphicon-ok"></span></td>
+		        <td><span class="glyphicon glyphicon-remove"></span></td>
+		      </tr>
+		      <tr>
+		        <td>Friends</td>
+		        <td>
+		          bangHero@ymail.sg
+		        </td>
+		        <td><span class="glyphicon glyphicon-ok"></span></td>
+		        <td><span class="glyphicon glyphicon-remove"></span></td>
+		      </tr>
+		    </table>
+		  </p>
+		</div>
+	</body>
+</html>
