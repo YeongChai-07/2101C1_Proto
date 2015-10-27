d8c5e2c18aad2b15b1d85537c82d4d560f3dac9e
YeongChai-07 <butchercai@gmail.com>
2015-10-27 23:07:41 +0800

    UPDATED the UI for Groups main page(with the Create New Group Form Controls).

--- ui/Groups.php
+++ ui/Groups.php
@@ -15,58 +15,84 @@
 			<li><a href="./Settings.html">Settings</a></li>
 		</ul>
 		<div class="container">
-		    <br/><br/><br/><br/>
-		    ====== Content Here ========
-		    Current Groups
-		    <table border = "1">
-		      <tr>
-		        <th>BANG BANG BANG</th>
-		        <th>&nbsp;</th>
-		      </tr>
-		      <tr>
-		        <td>Family</td>
-		        <td class ="glyphicon glyphicon-remove">
-		          <!-- Change X to icon -->
-		        </td>
-		      </tr>
-		      <tr>
-		        <td>Friends</td>
-		        <td class="glyphicon glyphicon-remove">
-		          <!-- Change X to icon -->
-		        </td>
-		      </tr>
-		      <tr>
-		        <td>&nbsp;</td>
-		        <td>&nbsp;</td>
-		      </tr>
-		    </table>
-		    <p>
-		      Received Invitations
-		    <table border = "1">
-		      <tr>
-		        <th>Group Name</th>
-		        <th>Email Address</th>
-		        <th>Accept Invitation</th>
-		        <th>Decline Invitation</th>
-		      </tr>
-		      <tr>
-		        <td>Family</td>
-		        <td>
-		          abc@email.com
-		        </td>
-		        <td><span class="glyphicon glyphicon-ok"></span></td>
-		        <td><span class="glyphicon glyphicon-remove"></span></td>
-		      </tr>
-		      <tr>
-		        <td>Friends</td>
-		        <td>
-		          bangHero@ymail.sg
-		        </td>
-		        <td><span class="glyphicon glyphicon-ok"></span></td>
-		        <td><span class="glyphicon glyphicon-remove"></span></td>
-		      </tr>
-		    </table>
-		  </p>
+		    <p>&nbsp;</p>
+			<div class="row">
+			    <div class="col-xs-6">
+				    <label for="currGrp_List">Current Groups</label>
+					<table border = "1" id="currGrp_List">
+					  <tr>
+						<th>BANG BANG BANG</th>
+						<th>&nbsp;</th>
+					  </tr>
+					  <tr>
+						<td>Family</td>
+						<td class ="glyphicon glyphicon-remove">
+						  <!-- Change X to icon -->
+						</td>
+					  </tr>
+					  <tr>
+						<td>Friends</td>
+						<td class="glyphicon glyphicon-remove">
+						  <!-- Change X to icon -->
+						</td>
+					  </tr>
+					  <tr>
+						<td>&nbsp;</td>
+						<td>&nbsp;</td>
+					  </tr>
+					</table>
+				</div>
+				<div class="col-xs-6">
+				    <form role="form">
+					    <div class="form-group">
+						    <label for="createGrp_Name">Create New</label>
+							<input type="text" class="form-control" id="createGrp_Name"
+							       placeholder="Enter new group name here"/>
+						</div>
+						<button type="submit" class="btn btn-default btn-md">Submit</button>
+					</form>
+				</div>
+			</div>
+			<div class="row">
+			    <div class="col-xs-12">&nbsp;</div>
+			</div>
+			<div class="row">
+			    <div class="col-xs-12">
+					<label for="testTableData">Received Invitations</label>
+						<table border = "1" id="testTableData">
+							<tr>
+								<th>Group Name</th>
+								<th>Email Address</th>
+								<th>Accept Invitation</th>
+								<th>Decline Invitation</th>
+							</tr>
+							<tr>
+								<td>Family</td>
+								<td>
+									abc@email.com
+								</td>
+								<td>
+									<span class="glyphicon glyphicon-ok"></span>
+								</td>
+								<td>
+									<span class="glyphicon glyphicon-remove"></span>
+								</td>
+							</tr>
+							<tr>
+								<td>Friends</td>
+								<td>
+									bangHero@ymail.sg
+								</td>
+								<td>
+									<span class="glyphicon glyphicon-ok"></span>
+								</td>
+								<td>
+									<span class="glyphicon glyphicon-remove"></span>
+								</td>
+							</tr>
+						</table>
+					</div>
+			</div>
 		</div>
 	</body>
 </html>
