58e491ec1c9bbcfd1ca9fb73ec2fc816a88f1cb8
NThanatos <asuramirai@gmail.com>
2015-11-01 12:42:58 +0800

    -update

--- ui/ShoppingListTest.php
+++ ui/ShoppingListTest.php
@@ -34,7 +34,7 @@ session_start();
             }
 
 
-            
+
 
             function updateList() {
                 var updateItemQty = document.forms["updateShoppingList"]["updateQty"].value;
@@ -58,7 +58,10 @@ session_start();
                     return true;
                 }
             }
-
+        function uploadList(){
+            alert("Shopping List online has been updated");
+        }
+            
         </script>
         <style>
             .container-fluid {
@@ -125,7 +128,7 @@ session_start();
 
     </head>
     <body class="container-fluid">
- <?php /*
+        <?php /*
           $_SESSION["USERNAME"] = "USERNAME";
           $_SESSION["PASSWORD"] = "PASSWORD";
 
@@ -138,161 +141,170 @@ session_start();
             <li><a href="./Settings.php">Settings</a></li>
         </ul>
 
-
         <!-- Top Left Container -->
-<?php
+        <?php
+        $selectedItem = "";
+        $selectedQuantity = "";
+        $selectedDescription = "";
+        $urgency = "-";
 
-$selectedItem = "";
-$selectedQuantity = "";
-$selectedDescription = "";
+        function item_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
 
-function item_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $selectedItem = $_POST["selectitem"];
+            $selectedQuantity = item_input($_POST["newShoppingListQty"]);
+            $selectedDescription = $_POST["newShoppingListDesc"];
+            $urgency = $_POST("urgencydate");
+        }
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $urgency = $_POST("urgencydate");
+        }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $selectedItem = $_POST["selectitem"];
-    $selectedQuantity = item_input($_POST["newShoppingListQty"]);
-    $selectedDescription = $_POST["newShoppingListDesc"];
-    $currentdt = date('Y-m-d H:i:s');
-}
-
-function addtolist(){
-    if (isset($_GET["selectitem"])){
-    echo '<tr>';
-    echo '<td>';
-    echo $selectedItem;
-    echo '</td>';
-    echo '<td>';
-    echo $selectedQuantity;
-    echo '</td>';
-    echo '</tr>';
-}}
-?>
+        function addtolist() {
+            if (isset($_GET["selectitem"])) {
+                echo '<tr>';
+                echo '<td>';
+                echo $selectedItem;
+                echo '</td>';
+                echo '<td>';
+                echo $selectedQuantity;
+                echo '</td>';
+                echo '</tr>';
+            }
+        }
+        ?>
         <div class="container" id="container-table">
-            <form name="updateShoppingList" action="ShoppingList.php" >
+            <form name="updateShoppingList" action="ShoppingList.php" method="post" role="form">
                 <br/><br/><br/><br/>
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            <h3>Shopping List</h3>
-                        </div>
-                        <table class="table" id="addList" onclick="displayList()">
-                            <tr>
-                                <td><b>Item</b></td>
-                                <td><b>Description</b></td>
-                                <td><b>Quantity</b></td>
-                                <td><b>Delete?</b></td>
-                            </tr>
-                            <tr>
-                                <td>Milo</td>
-                                <td><input type="text" class="text-primary text-desc" value="Energy"></td>
-                                <td><input type="text" class="text-primary text-qty" value="3"></td>
-                                <td><button name="deleteFromShoppingList" class="btn btn-danger btn-del" value="milo">X</button></td>
-                            </tr>
-                            <tr>
-                                <td>Beer</td>
-                                <td><input type="text" class="text-primary text-desc" value="Tiger"></td>
-                                <td><input type="text" class="text-primary text-qty"  value="4"></td>
-                                <td><button name="deleteFromShoppingList" class="btn btn-danger btn-del" value="beer">X</button></td>
-                            </tr>
-                            <tr>
-                                <td>Tea</td>
-                                <td><input type="text" class="text-primary text-desc" value="Pokka"></td>
-                                <td><input type="text" class="text-primary text-qty"  value="1"></td>
-                                <td><button name="deleteFromShoppingList" class="btn btn-danger btn-del" value="tea">X</button></td>
-                            </tr>                            
-                            <?php                        
-                            if (isset($_POST["selectitem"])) {
-                                
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3>Shopping List</h3>
+                        <h4 style="float:right; color: black">Urgency date: <?php echo $urgency ?></h4>
+                    </div>
+                    <table class="table" id="addList" onclick="displayList()">
+                        <tr>
+                            <td><b>Item</b></td>
+                            <td><b>Description</b></td>
+                            <td><b>Quantity</b></td>
+                            <td><b>Delete?</b></td>
+                        </tr>
+                        <tr>
+                            <td>Milo</td>
+                            <td><input type="text" class="text-primary text-desc" value="Energy"></td>
+                            <td><input type="text" class="text-primary text-qty" value="3"></td>
+                            <td><button name="deleteFromShoppingList" class="btn btn-danger btn-del" value="milo">X</button></td>
+                        </tr>
+                        <tr>
+                            <td>Beer</td>
+                            <td><input type="text" class="text-primary text-desc" value="Tiger"></td>
+                            <td><input type="text" class="text-primary text-qty"  value="4"></td>
+                            <td><button name="deleteFromShoppingList" class="btn btn-danger btn-del" value="beer">X</button></td>
+                        </tr>
+                        <tr>
+                            <td>Tea</td>
+                            <td><input type="text" class="text-primary text-desc" value="Pokka"></td>
+                            <td><input type="text" class="text-primary text-qty"  value="1"></td>
+                            <td><button name="deleteFromShoppingList" class="btn btn-danger btn-del" value="tea">X</button></td>
+                        </tr>                            
+                        <?php
+                        if (isset($_POST["selectitem"])) {
+
                             echo '<tr>';
                             echo '<td>';
                             echo $selectedItem;
                             echo '</td>';
-                            echo '<td><input type="text" class="text-primary text-desc" value=" '.$selectedDescription. '">';
+                            echo '<td><input type="text" class="text-primary text-desc" value=" ' . $selectedDescription . '">';
                             echo '</td>';
-                            echo '<td><input type="text" class="text-primary text-qty" value=" '.$selectedQuantity. '">';
+                            echo '<td><input type="text" class="text-primary text-qty" value=" ' . $selectedQuantity . '">';
                             echo '</td>';
-                            echo '<td><button name="deleteFromShoppingList" class="btn btn-danger btn-del" value=" '.$selectedItem.'">X</button></td>';
+                            echo '<td><button name="deleteFromShoppingList" class="btn btn-danger btn-del" value=" ' . $selectedItem . '">X</button></td>';
                             echo '</tr>';
-}
-?>
-                        </table>
+                        }
+                        ?>
+                        <tr>
+                            <td>
+                                <input type="date" name="urgencydate">
+                            </td>
+                        </tr>
+                    </table>
+
+                </div>
+
+                <!-- Bottom Left Container -->            
+
+                <div class="container">
+                    <div class="row btn-update-row">
+
+                        <button class="btn btn-primary" type="submit" onsubmit="return updateList();" onclick="uploadList();">Update List</button>
 
                     </div>
 
-                    <!-- Bottom Left Container -->            
+                    <div class="row">
+                        <div class="col-sm-4"><h3 class="h3">Set Urgency Dateline</h3></div>
+                    </div>
 
-                    <div class="container">
-                        <div class="row btn-update-row">
-
-                            <button class="btn btn-primary" type="submit" onsubmit="return updateList();">Update List</button>
-
-                        </div>
-
-                        <div class="row">
-                            <div class="col-sm-4"><h3 class="h3">Set Urgency Dateline</h3></div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-sm-1"><h3 class="h3">By : </h3></div>
-                            <div class="col-sm-1" >
-                                <input id="datepick" size="20">
-                                <script type="text/javascript">
+                    <div class="row">
+                        <div class="col-sm-1"><h3 class="h3">By : </h3></div>
+                        <div class="col-sm-1" >
+                            <input id="datepick" size="20">
+                            <script type="text/javascript">
                                     new datepickr('datepick');</script>
-                            </div>
-                        </div>
-
-                    </div>
-                </form>
-            </div>
-        
-            <!-- Right Container -->
-            <div class="container" id="container-add">
-                <h2 class="h2" id="h2-add">List Name</h2>
-
-                <form name="newShoppingListItemForm" onsubmit="return checkEmptyInput();" method="post" role="form" >
-                    <h3 class="h3">Add things to buy :</h3>
-                    <br/>
-                    <script type="text/javascript">
-                            $(document.body).on('click', '.dropdown-menu li', function (event) {
-
-                                var $target = $(event.currentTarget);
-
-                                $target.closest('.btn-group')
-                                        .find('[data-bind="label"]').text($target.text())
-                                        .end()
-                                        .children('.dropdown-toggle').dropdown('toggle');
-
-                                return false;
-
-                            });
-                    </script>
-                    <div class="btn-group"  >                       
-                        <select class="btn btn-default dropdown-toggle form-control" id="dropdown-add" name="selectitem">
-                            <option value="Pizza">Pizza</option>
-                            <option value="Beer">Beer</option>
-                            <option value="Tea">Tea</option>
-                            <option value="Meat">Meat</option>
-                        </select>
-                    </div>
-                    <br /><br /><br /><br /><br />
-
-                    <div class="row">
-                        <div class="col-sm-4"><h3 class="h3">Quantity : </h3></div>
-                        <div class="col-sm-5" id="qtyCol" ><input type="text" name="newShoppingListQty" class="text-primary" id="newShoppingListQty"></div>
-
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-4"><h3 class="h3">Description : </h3></div>
-                        <div class="col-sm-5" id="descCol"><textarea id="descTxtArea" name="newShoppingListDesc" class="text-primary" id="newShoppingListDesc"></textarea></div>
-                        <div class="col-sm-4" id="addBtnCol">
-                            <button type="submit" class="btn btn-primary" id="addBtn">Add</button>
                         </div>
                     </div>
-                </form>
+
+                </div>
+            </form>
+        </div>
+
+        <!-- Right Container -->
+        <div class="container" id="container-add">
+            <h2 class="h2" id="h2-add">List Name</h2>
+
+            <form name="newShoppingListItemForm" onsubmit="return checkEmptyInput();" method="post" role="form" >
+                <h3 class="h3">Add things to buy :</h3>
+                <br/>
+                <script type="text/javascript">
+                    $(document.body).on('click', '.dropdown-menu li', function (event) {
+
+                        var $target = $(event.currentTarget);
+
+                        $target.closest('.btn-group')
+                                .find('[data-bind="label"]').text($target.text())
+                                .end()
+                                .children('.dropdown-toggle').dropdown('toggle');
+
+                        return false;
+
+                    });
+                </script>
+                <div class="btn-group"  >                       
+                    <select class="btn btn-default dropdown-toggle form-control" id="dropdown-add" name="selectitem">
+                        <option value="Pizza">Pizza</option>
+                        <option value="Beer">Beer</option>
+                        <option value="Tea">Tea</option>
+                        <option value="Meat">Meat</option>
+                    </select>
+                </div>
+                <br /><br /><br /><br /><br />
+
+                <div class="row">
+                    <div class="col-sm-4"><h3 class="h3">Quantity : </h3></div>
+                    <div class="col-sm-5" id="qtyCol" ><input type="text" name="newShoppingListQty" class="text-primary" id="newShoppingListQty"></div>
+
+                </div>
+                <div class="row">
+                    <div class="col-sm-4"><h3 class="h3">Description : </h3></div>
+                    <div class="col-sm-5" id="descCol"><textarea id="descTxtArea" name="newShoppingListDesc" class="text-primary" id="newShoppingListDesc"></textarea></div>
+                    <div class="col-sm-4" id="addBtnCol">
+                        <button type="submit" class="btn btn-primary" id="addBtn">Add</button>
+                    </div>
+                </div>
+            </form>
 
         </div>
 
