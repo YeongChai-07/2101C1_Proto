203fb2b35f236ea5ef762193d3c8527437ed4198
NThanatos <asuramirai@gmail.com>
2015-11-25 21:24:20 +0800

    leave grp bug fix

--- ui/ExitGroup.php
+++ ui/ExitGroup.php
@@ -11,17 +11,19 @@ echo $email;
 
 $sql1 ="SELECT * FROM groups WHERE email = '".$email."' AND groupName = '".$name."' AND groupCreator = '".$creator."'";
 if ($result1 = mysqli_query($connection, $sql1)){
+    while ($row = mysqli_fetch_assoc($result1)) {
     $sql3 = "DELETE FROM groups WHERE groupName = '".$name."'";
     if ($result3 = mysqli_query($connection, $sql3)){
-        echo 'success delete all';
+            echo 'success delete all'; 
+    }   
     }
 }
-else {
+
     $sql2 = "DELETE FROM groups WHERE email = '".$email."' AND groupName = '".$name."'";
 
 if ($result2 = mysqli_query($connection, $sql2)){
     echo 'success';
-}
+
 }
 	header('location: ../ui/Groups.php');
 ?>
--- ui/withingroupdelete.php
+++ ui/withingroupdelete.php
@@ -6,22 +6,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = ($_POST["email2"]);
     $name = ($_POST["grpname2"]);
 }
-
+echo $email;
 
 $sql1 ="SELECT * FROM groups WHERE email = '".$email."' AND groupName = '".$name."'AND groupCreator = '".$creator."'" ;
 if ($result1 = mysqli_query($connection, $sql1)){
+    while ($row = mysqli_fetch_assoc($result1)) {
     $sql3 = "DELETE FROM groups WHERE groupName = '".$name."'";
     if ($result3 = mysqli_query($connection, $sql3)){
-        echo 'success delete all';
+            echo 'success delete all'; 
+    }   
     }
 }
-else {
+
     $sql2 = "DELETE FROM groups WHERE email = '".$email."' AND groupName = '".$name."'";
 
 if ($result2 = mysqli_query($connection, $sql2)){
     echo 'success';
 }
-}
+
 if($email != $sessionemail ){
     header('location: ../ui/GroupInfo.php?id='.$name);
 }
